c8c9cab4c67a1bca5634e98595c069018bb24bfa
TolyaHuligan <66628747+StupidStrawberry@users.noreply.github.com>
2024-11-07 22:51:25 +0700

    забыл дописать тесты

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ def rot90(matrix):
 
 def rot270(matrix):
     return [list(row) for row in list(zip(*matrix))[::-1]]
+
+
+if __name__ == "__main__":
+    a = [[1, 2], [3, 4]]
+    print(rot90(a))
+    print(rot270(a))
