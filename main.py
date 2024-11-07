05c4be5f8f911f60d722d31ddab82e19e04ed93b
TolyaHuligan <66628747+StupidStrawberry@users.noreply.github.com>
2024-11-07 22:55:26 +0700

    3 - функция для суммы больших чисел

--- main.py
+++ main.py
@@ -6,7 +6,22 @@ def rot270(matrix):
     return [list(row) for row in list(zip(*matrix))[::-1]]
 
 
+def big_sum(mas_a, mas_b):
+    int_mas_a = int(''.join(map(str, mas_a)))
+    int_mas_b = int(''.join(map(str, mas_b)))
+    x = int_mas_a + int_mas_b
+    mas_x = []
+    while x > 0:
+        mas_x.append(x % 10)
+        x //= 10
+    return mas_x
+
+
 if __name__ == "__main__":
     a = [[1, 2], [3, 4]]
     print(rot90(a))
     print(rot270(a))
+    x = [1, 2, 3]
+    y = [3, 2, 1]
+    print(big_sum(x,y))
+    print(321+123)
