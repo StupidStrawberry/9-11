40f18231bd9ba9a56b90b9b31df67887757baed8
TolyaHuligan <66628747+StupidStrawberry@users.noreply.github.com>
2024-11-07 23:21:22 +0700

    4 - добавленна разность между большими  числами

--- main.py
+++ main.py
@@ -9,19 +9,34 @@ def rot270(matrix):
 def big_sum(mas_a, mas_b):
     int_mas_a = int(''.join(map(str, mas_a)))
     int_mas_b = int(''.join(map(str, mas_b)))
-    x = int_mas_a + int_mas_b
+    z = int_mas_a + int_mas_b
     mas_x = []
-    while x > 0:
-        mas_x.append(x % 10)
-        x //= 10
+    while z > 0:
+        mas_x.append(z % 10)
+        z //= 10
+    mas_x.reverse()
+    return mas_x
+
+
+def big_diff(mas_a, mas_b):
+    int_mas_a = int(''.join(map(str, mas_a)))
+    int_mas_b = int(''.join(map(str, mas_b)))
+    z = int_mas_a - int_mas_b
+    mas_x = []
+    while z > 0:
+        mas_x.append(z % 10)
+        z //= 10
+    mas_x.reverse()
     return mas_x
 
 
 if __name__ == "__main__":
-    a = [[1, 2], [3, 4]]
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     print(rot90(a))
     print(rot270(a))
     x = [1, 2, 3]
     y = [3, 2, 1]
-    print(big_sum(x,y))
+    print(big_sum(x, y))
     print(321+123)
+    print(big_diff(y, x))
+    print(321 - 123)
