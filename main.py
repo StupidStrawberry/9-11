b8037be85d87ee76477ec61b1d7ada6d96771b6a
TolyaHuligan <66628747+StupidStrawberry@users.noreply.github.com>
2024-11-07 23:51:41 +0700

    5 - количество совпадающих числе в массивах

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+
+
 def rot90(matrix):
     return [list(reversed(col)) for col in zip(*matrix)]
 
@@ -9,11 +12,11 @@ def rot270(matrix):
 def big_sum(mas_a, mas_b):
     int_mas_a = int(''.join(map(str, mas_a)))
     int_mas_b = int(''.join(map(str, mas_b)))
-    z = int_mas_a + int_mas_b
+    x = int_mas_a + int_mas_b
     mas_x = []
-    while z > 0:
-        mas_x.append(z % 10)
-        z //= 10
+    while x > 0:
+        mas_x.append(x % 10)
+        x //= 10
     mas_x.reverse()
     return mas_x
 
@@ -21,13 +24,26 @@ def big_sum(mas_a, mas_b):
 def big_diff(mas_a, mas_b):
     int_mas_a = int(''.join(map(str, mas_a)))
     int_mas_b = int(''.join(map(str, mas_b)))
-    z = int_mas_a - int_mas_b
+    x = int_mas_a - int_mas_b
     mas_x = []
-    while z > 0:
-        mas_x.append(z % 10)
-        z //= 10
+    while x > 0:
+        mas_x.append(x % 10)
+        x //= 10
     mas_x.reverse()
     return mas_x
+
+
+def how_much_same(mas_a, mas_b):
+    k = 0
+    for i in range(len(mas_a)):
+        a = abs(mas_a[i])
+        for j in range(len(mas_b)):
+            b = abs(mas_b[j])
+            if a == b:
+                k += 1
+    return k
+
+
 
 
 if __name__ == "__main__":
@@ -37,6 +53,7 @@ if __name__ == "__main__":
     x = [1, 2, 3]
     y = [3, 2, 1]
     print(big_sum(x, y))
-    print(321+123)
+    print(321 + 123)
     print(big_diff(y, x))
     print(321 - 123)
+    print(how_much_same(x,y))
