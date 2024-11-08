09b020ff4bb440ccff4b5b691b3657eff0a39e48
TolyaHuligan <66628747+StupidStrawberry@users.noreply.github.com>
2024-11-08 13:13:43 +0700

    11 - сделано менб (финал)

--- main.py
+++ main.py
@@ -26,10 +26,16 @@ def big_diff(mas_a, mas_b):
     int_mas_b = int(''.join(map(str, mas_b)))
     x = int_mas_a - int_mas_b
     mas_x = []
+    minus = 0
+    if x < 0:
+        x = abs(x)
+        minus = 1
     while x > 0:
         mas_x.append(x % 10)
         x //= 10
     mas_x.reverse()
+    if minus:
+        mas_x[0] = mas_x[0]*(-1)
     return mas_x
 
 
@@ -112,7 +118,40 @@ def gen_or_get_mat():
         print("Неверные данные")
 
 
+def menu(point):
+    if point == 1:
+        mas_a = gen_or_get_mas()
+        mas_b = gen_or_get_mas()
+        print("Сумма (1) или разность (2)")
+        x = int(input())
+        if x == 1:
+            print(big_sum(mas_a, mas_b))
+        elif x== 2:
+            print(big_diff(mas_a, mas_b))
+        else:
+            print("Error")
+    elif point == 2:
+        mat = gen_or_get_mat()
+        print("Повернуть матрицу на право (1) или на лево (2)")
+        x = int(input())
+        if x == 1:
+            print(rot90(mat))
+        elif x == 2:
+            print(rot270(mat))
+        else:
+            print("Error")
+    elif point == 3:
+        mas_a = gen_or_get_mas()
+        mas_b = gen_or_get_mas()
+        print(how_much_same(mas_a, mas_b))
+    elif point == 4:
+        exit()
+    else:
+        print("Error")
+
 
 if __name__ == "__main__":
-    print(gen_or_get_mas())
-    print(gen_or_get_mat())
+    while True:
+        print("Задачи\n 1. Входные данные: 2 массива с числами одинакового размера. Нужно произвести сумму чисел из массивов, первый массив должен быть отсортирован в порядке убывания, второй в порядке возрастания. Если числа в массивах совпадают, их сумма будет равна нулю. Конечный массив нужно отсортировать в порядке возрастания.\n2. Входные данные: матрица N на M. Требуется повернуть матрицу на 90 градусов против часовой или по часовой.\n 3. Входные данные: 2 массива с цифрами, каждый представляет собой большое число. Нужно произвести сумму или разность массивов. 4. Закончить работу программы")
+        print("Выберите пункт меню")
+        menu(int(input()))
